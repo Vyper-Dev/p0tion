504519b8fb7fcc8504a3648979e2ee7d20923e34
Jack Sangster <61627524+jacksangster5@users.noreply.github.com>
2021-02-10 21:28:25 -0400

    Creation

---
+++ v1rosaSn0w/v1rosaSn0w/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  v1rosaSn0w
+//
+//  Created by Jack Sangster on 2021-02-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ v1rosaSn0w/v1rosaSn0w/v1rosaSn0wApp.swift
@@ -0,0 +1,17 @@
+//
+//  v1rosaSn0wApp.swift
+//  v1rosaSn0w
+//
+//  Created by Jack Sangster on 2021-02-10.
+//
+
+import SwiftUI
+
+@main
+struct v1rosaSn0wApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
