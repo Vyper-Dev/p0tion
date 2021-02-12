8a3abb8c64f9cba5f4f78b5b1442a7cf9d9c0da5
K0stad1n <>
2021-02-12 17:15:14 +0100

    Added package and import statement.

--- p0tion/p0tion/ViewController.swift
+++ p0tion/p0tion/ViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
+import cicutavirosawrap
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
         
         let Title = UILabel(frame: CGRect(x:0, y:200, width: 200, height: 100))
