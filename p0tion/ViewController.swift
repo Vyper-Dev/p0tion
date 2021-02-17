15d0cf0eabd7612782845965f2b4044d0b06a4d5
Jack Sangster <61627524+jacksangster5@users.noreply.github.com>
2021-02-16 20:41:50 -0400

    fixes

--- p0tion/ViewController.swift
+++ p0tion/ViewController.swift
@@ -11,7 +11,7 @@ import WebKit
 import Foundation
 
 class ViewController: UIViewController, WKUIDelegate {
-   var webView: WKWebView!
+   /*var webView: WKWebView!
    override func viewDidLoad() {
       super.viewDidLoad()
       let myURL = URL(string:"https://www.apple.com")
@@ -23,5 +23,49 @@ class ViewController: UIViewController, WKUIDelegate {
       webView = WKWebView(frame: .zero, configuration: webConfiguration)
       webView.uiDelegate = self
       view = webView
-   }
+   }*/
+    
+    @objc func RunExploit(_ sender: UIButton) {
+        DispatchQueue.global(qos: .background).async {
+            cicuta_virosa()
+            sandbox()
+        }
+    }
+        
+    @objc func testRootFunc(_ sender: UIButton) {
+        sandbox()
+    }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+            
+        let Title = UILabel(frame: CGRect(x:0, y:200, width: 200, height: 100))
+        Title.text = "p0tion"
+        Title.font = UIFont.boldSystemFont(ofSize: 50)
+        Title.textColor = .white
+        Title.textAlignment = .center
+        Title.center.x = self.view.center.x
+        view.addSubview(Title)
+            
+        let RunButton = UIButton(frame: CGRect(x: 0, y: 0, width: 250, height: 250))
+        RunButton.backgroundColor = .white
+        RunButton.center = self.view.center
+        RunButton.layer.cornerRadius = 125
+        RunButton.setTitle("Jailbreak", for: .normal)
+        RunButton.setTitleColor(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), for: .normal)
+        RunButton.titleLabel?.font = UIFont.systemFont(ofSize: 25)
+        RunButton.addTarget(self, action: #selector(RunExploit(_:)), for: .touchUpInside)
+        view.addSubview(RunButton)
+            
+        let TestRoot = UIButton(frame: CGRect(x: 0, y: 40, width: 150, height: 100))
+        TestRoot.backgroundColor = .white
+        TestRoot.center.x = self.view.center.x
+        TestRoot.layer.cornerRadius = 45
+        TestRoot.setTitle("Test Root", for: .normal)
+        TestRoot.setTitleColor(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), for: .normal)
+        TestRoot.titleLabel?.font = UIFont.systemFont(ofSize: 25)
+        TestRoot.addTarget(self, action: #selector(testRootFunc(_:)), for: .touchUpInside)
+        view.addSubview(TestRoot)
+    }
 }
